eaa242c9a6b19bc4207ff431279b8a772b673c52
NeltonMenata <neltonmenataditec@gmail.com>
2022-11-26 16:43:05 +0100

    Alterando tela home

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, unused_element
-
 import 'package:elokyetu/screens/salas/arquitetura/arquitetura_screen.dart';
 import 'package:elokyetu/screens/salas/gastronomia/post_gastronomy_controller.dart';
+import 'package:elokyetu/util/size/size.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -12,7 +12,6 @@ import 'package:elokyetu/components/main_drawer.dart';
 import 'package:elokyetu/screens/salas/arte/arte_page.dart';
 import 'package:elokyetu/screens/salas/musica/musica_view/musica_view.dart';
 import 'package:elokyetu/theme/colors.dart';
-import 'package:elokyetu/util/size/size.dart';
 import '../salas/gastronomia/categoria_gastronomia.dart';
 
 class HomePage extends StatefulWidget {
@@ -98,49 +97,70 @@ class _HomePageState extends State<HomePage> {
         drawer: MainDrawer(),
         body: SingleChildScrollView(
           child: Column(
-            mainAxisSize: MainAxisSize.min,
+            //mainAxisSize: MainAxisSize.min,
             children: [
-              SizedBox(height: 8),
-              Grid2Menu(
-                url1: 'assets/categoria/gastro.png',
-                name1: "gastronomia",
-                color1: Colors.blue,
-                action1: () {
-                  Get.to(CategoriasGastronomia());
-                },
-                url2: 'assets/categoria/saude.jpg',
-                name2: "saúde",
-                color2: Colors.green,
-                action2: () {},
-              ),
-              SizedBox(height: 8),
-              Grid2Menu(
-                url1: 'assets/categoria/arte.jpg',
-                name1: "arte",
-                color1: Colors.orange,
-                action1: () {
-                  Get.to(() => ArteView());
-                },
-                url2: 'assets/categoria/estilo.png',
-                name2: "vida e estilo",
-                color2: Colors.red,
-                action2: () {},
-              ),
-              SizedBox(height: 8),
-              Grid2Menu(
-                url1: 'assets/categoria/music-logo.jpg',
-                name1: "EloTV",
-                color1: Colors.yellow,
-                action1: () {
-                  print("Você clicou na aba musica");
-                  Get.to(() => MusicaView());
-                },
-                url2: 'assets/categoria/diverso.png',
-                name2: "Arquitetura",
-                color2: Colors.teal,
-                action2: () {
-                  Get.to(() => ArquiteturaScreen());
-                },
+              Container(
+                color: Colors.black,
+                width: Get.width,
+                padding: const EdgeInsets.all(10),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Grid2Menu(
+                        url1: 'assets/categoria/gastro.png',
+                        name1: "gastronomia",
+                        color1: Colors.blue,
+                        action1: () {
+                          Get.to(CategoriasGastronomia());
+                        },
+                      ),
+                      const SizedBox(width: 10),
+                      Grid2Menu(
+                        url1: 'assets/categoria/saude.jpg',
+                        name1: "saúde",
+                        color1: Colors.green,
+                        action1: () {},
+                      ),
+                      const SizedBox(width: 10),
+                      Grid2Menu(
+                        url1: 'assets/categoria/arte.jpg',
+                        name1: "arte",
+                        color1: Colors.orange,
+                        action1: () {
+                          Get.to(() => ArteView());
+                        },
+                      ),
+                      const SizedBox(width: 10),
+                      Grid2Menu(
+                        url1: 'assets/categoria/estilo.png',
+                        name1: "vida e estilo",
+                        color1: Colors.red,
+                        action1: () {},
+                      ),
+                      const SizedBox(width: 10),
+                      Grid2Menu(
+                        url1: 'assets/categoria/music-logo.jpg',
+                        name1: "EloTV",
+                        color1: Colors.yellow,
+                        action1: () {
+                          print("Você clicou na aba musica");
+                          Get.to(() => MusicaView());
+                        },
+                      ),
+                      const SizedBox(width: 10),
+                      Grid2Menu(
+                        url1: 'assets/categoria/diverso.png',
+                        name1: "Arquitetura",
+                        color1: Colors.teal,
+                        action1: () {
+                          Get.to(() => ArquiteturaScreen());
+                        },
+                      )
+                    ],
+                  ),
+                ),
               ),
               PostWidget(),
             ],
@@ -165,90 +185,40 @@ class Grid2Menu extends StatelessWidget {
   const Grid2Menu({
     Key? key,
     required this.url1,
-    required this.url2,
     required this.name1,
-    required this.name2,
     required this.color1,
-    required this.color2,
     required this.action1,
-    required this.action2,
   }) : super(key: key);
 
   final String url1;
-  final String url2;
+
   final String name1;
-  final String name2;
+
   final Color color1;
-  final Color color2;
+
   final Function action1;
-  final Function action2;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        SizedBox(
-          width: larguraPor(48, context),
-          height: larguraPor(45, context),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: GridTile(
-              child: GestureDetector(
-                onTap: () {
-                  action1();
-                },
-                child: Image.asset(
-                  url1,
-                  fit: BoxFit.fill,
-                ),
-              ),
-              footer: GridTileBar(
-                backgroundColor: color1,
-                title: Text(
-                  name1.tr.capitalize!,
-                  style: TextStyle(
-                    fontSize: 17,
-                    fontFamily: 'Times New Roman',
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          ),
-        ),
-        SizedBox(
-          width: larguraPor(48, context),
-          height: larguraPor(45, context),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: GridTile(
-              child: GestureDetector(
-                onTap: () {
-                  action2();
-                },
-                child: Image.asset(
-                  url2,
-                  fit: BoxFit.fitHeight,
-                ),
-              ),
-              footer: GridTileBar(
-                backgroundColor: color2,
-                title: Text(
-                  name2.tr.capitalize!,
-                  style: TextStyle(
-                    fontSize: 17,
-                    fontFamily: 'Times New Roman',
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
+    return GestureDetector(
+      onTap: () {
+        action1();
+      },
+      child: Column(
+        children: [
+          CircleAvatar(
+              radius: larguraPor(10, context),
+              backgroundColor: Colors.blue,
+              child: CircleAvatar(
+                  radius: larguraPor(9, context),
+                  backgroundImage: AssetImage(url1))),
+          Text(name1.tr.capitalize!,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 17))
+        ],
+      ),
     );
   }
 }
